5327f4f86986cd002bbf214f22676bdc0e7fc70b
Sergio Alvarez <salvare736@gmail.com>
2021-04-23 16:06:23 -0400

    Completed interview questions

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,15 +1,36 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import BubblePage from "./BubblePage";
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import BubblePage from './BubblePage';
 
-test("Renders BubblePage without errors", () => {
-  // Finish this test
+const testData = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff",
+    },
+    id: 1,
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc",
+    },
+    id: 2,
+  },
+  {
+    color: "aqua",
+    code: {
+      hex: "#00ffff",
+    },
+    id: 3,
+  }
+];
+
+test('Renders BubblePage without errors', () => {
+
 });
 
-test("Fetches data and renders the bubbles on mounting", () => {
-  // Finish this test
-});
+test('Fetches data and renders the bubbles on mounting', async () => {
 
-//Task List
-//1. Setup test for basic rendering of component
-//2. Setup test for initial rendering of bubbles on loading
+});
